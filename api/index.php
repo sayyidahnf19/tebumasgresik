96ea9a8e07d65d867dd59774afdccf2337f55231
sayyidahnf19 <sayyidahnf19@gmail.com>
2026-05-04 15:43:13 +0700

    rill

--- api/index.php
+++ api/index.php
@@ -1,17 +1,19 @@
 <?php
 
+$_ENV['APP_ENV'] = $_ENV['APP_ENV'] ?? 'production';
+
 // Vercel Serverless Function Entry Point for Laravel
-// This file routes all requests to the Laravel application
+require __DIR__ . '/../vendor/autoload.php';
 
-define('LARAVEL_START', microtime(true));
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Require the Bootstrap file
-require __DIR__ . '/../bootstrap/app.php';
-
-// Handle the request through Laravel
 $kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
+
 $response = $kernel->handle(
     $request = \Illuminate\Http\Request::capture()
 );
+
 $response->send();
+
 $kernel->terminate($request, $response);
+
