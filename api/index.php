1408db750f6efd1e26b1882c5845ba8f24533ebf
sayyidahnf19 <sayyidahnf19@gmail.com>
2026-05-05 21:32:15 +0700

    api

--- api/index.php
+++ api/index.php
@@ -1,19 +1,6 @@
 <?php
-
-$_ENV['APP_ENV'] = $_ENV['APP_ENV'] ?? 'production';
-
 // Vercel Serverless Function Entry Point for Laravel
 require __DIR__ . '/../public/index.php';
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = \Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
 
