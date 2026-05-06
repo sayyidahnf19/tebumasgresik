b41fa7da207ffac423157a4a9d69397655d8c716
sayyidahnf19 <sayyidahnf19@gmail.com>
2026-05-06 20:42:25 +0700

    navbar

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     base: process.env.NODE_ENV === 'production' ? '/build/' : '/',
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
