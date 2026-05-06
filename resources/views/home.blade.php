eeeee4e71baed37cedd1f1c950445c48d4faeef0
sayyidahnf19 <sayyidahnf19@gmail.com>
2026-05-06 11:24:02 +0700

    perbaikan design mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-5 col-lg-6 order-0 order-md-1 text-end">
-                <img class="pt-7 pt-md-0 hero-img" src="{{ asset('img/hero/hero-img.png') }}" alt="hero-header" style="max-width: 60%; width: auto; height: auto;" />
+                <img class="pt-7 pt-md-0 hero-img " src="{{ asset('img/hero/hero-img.png') }}" alt="hero-header" style="max-width: 60%; width: auto; height: auto;" />
             </div>
             <div class="col-md-7 col-lg-6 text-md-start text-center py-6">
                 <h4 class="fw-bold text-danger mb-3">{{ $heroTitle ?? 'Produsen Songkok Berkualitas dari Gresik' }}</h4>
