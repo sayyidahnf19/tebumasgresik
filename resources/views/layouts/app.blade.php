9e492600aee0f69b867c05efed9572225563e131
sayyidahnf19 <sayyidahnf19@gmail.com>
2026-05-13 15:55:19 +0700

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en-US" dir="ltr">
 
 <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,12 +16,12 @@
     <!-- ===============================================-->
     <!--    Favicons-->
     <!-- ===============================================-->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicons/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicons/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicons/favicon-16x16.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/favicons/favicon.ico') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/LogoTM.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/LogoTM.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/LogoTM.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/LogoTM.png') }}">
     <link rel="manifest" href="{{ asset('img/favicons/manifest.json') }}">
-    <meta name="msapplication-TileImage" content="{{ asset('img/favicons/mstile-150x150.png') }}">
+    <meta name="msapplication-TileImage" content="{{ asset('img/LogoTM.png') }}">
     <meta name="theme-color" content="#ffffff">
 
     <!-- ===============================================-->
